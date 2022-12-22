6d205d39908b527ef6889669ef7b28bcd14a877b
youssefjn <youssefjneyah@gmail.com>
2022-12-22 12:28:52 +0100

    minor update

--- coffeeFrontend/src/app/app-routing.module.ts
+++ coffeeFrontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { AdminComponent } from './admin/admin.component';
+import { AppComponent } from './app.component';
+const routes: Routes = [
+  {path:'home' ,component:AppComponent},
+  {path:'admin' ,component:AdminComponent},
+  {path: '', redirectTo: 'home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
